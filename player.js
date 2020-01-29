dc64d27deecfd173e0cb7f7a5420be1d99c524a8
Yubo Wang <yubow2@uw.edu>
2020-01-29 13:53:38 -0800

    updated main character animation

--- player.js
+++ player.js
@@ -10,25 +10,34 @@ class Player {
         this.yvelocity = 0;
         this.spritesheet = spritesheet;
         this.currentKey = 'S';
+
+        this.walkAnimationUp = new Animation(spritesheet, 0, 0, 64, 64, 0.15, 9, true, false);
+        this.walkAnimationDown = new Animation(spritesheet, 0, 128, 64, 64, 0.15, 9, true, false);
+        this.walkAnimationLeft = new Animation(spritesheet, 0, 64, 64, 64, 0.15, 9, true, false);
+        this.walkAnimationRight = new Animation(spritesheet, 0, 192, 64, 64, 0.15, 9, true, false);
     }
 
     draw() {
         if(this.currentKey === 'W' || this.currentKey === 'w') {
-            this.ctx.drawImage(this.spritesheet, 0, 0, 
-                this.width, this.height, this.x, this.y, 
-                this.width, this.height);
+            // this.ctx.drawImage(this.spritesheet, 0, 0, 
+            //     this.width, this.height, this.x, this.y, 
+            //     this.width, this.height);
+            this.walkAnimationUp.drawFrame(this.game.clockTick, this.ctx, this.x, this.y, 1);
         } else if(this.currentKey === 'A' || this.currentKey === 'a') {
-            this.ctx.drawImage(this.spritesheet, 0, 1 * this.height, 
-                this.width, this.height, this.x, this.y, 
-                this.width, this.height);
+            // this.ctx.drawImage(this.spritesheet, 0, 1 * this.height, 
+            //     this.width, this.height, this.x, this.y, 
+            //     this.width, this.height);
+            this.walkAnimationLeft.drawFrame(this.game.clockTick, this.ctx, this.x, this.y, 1);
         } else if(this.currentKey === 'S' || this.currentKey === 's' || this.currentKey === '' || this.currentKey === undefined) {
-            this.ctx.drawImage(this.spritesheet, 0, 2 * this.height, 
-                this.width, this.height, this.x, this.y, 
-                this.width, this.height);
+            // this.ctx.drawImage(this.spritesheet, 0, 2 * this.height, 
+            //     this.width, this.height, this.x, this.y, 
+            //     this.width, this.height);
+            this.walkAnimationDown.drawFrame(this.game.clockTick, this.ctx, this.x, this.y, 1);
         } else {
-            this.ctx.drawImage(this.spritesheet, 0, 3 * this.height, 
-                this.width, this.height, this.x, this.y, 
-                this.width, this.height);
+            // this.ctx.drawImage(this.spritesheet, 0, 3 * this.height, 
+            //     this.width, this.height, this.x, this.y, 
+            //     this.width, this.height);
+            this.walkAnimationRight.drawFrame(this.game.clockTick, this.ctx, this.x, this.y, 1);
         }
     }
 
